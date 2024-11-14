649bed29425c352888d3c8449c2a802f85f9597f
annkits <annpodolskaya0965@gmail.com>
2024-11-14 21:00:54 +0700

    Create task1.py

---
+++ lab5/task1.py
@@ -0,0 +1,8 @@
+days = int(input("Введите количество дней: "))
+hours = int(input("Введите количество часов: "))
+minutes = int(input("Введите количество минут: "))
+seconds = int(input("Введите количество секунд: "))
+
+total_seconds = (days * 24 * 3600) + (hours * 3600) + (minutes * 60) + (seconds)
+
+print(f"Общее количество секунд: {total_seconds}")
